7bbbb7fc5e6d4b9a7d5147c9c819464fb9a61d08
Brian Jones <bjones@mcclatchyinteractive.com>
2011-10-25 23:03:26 -0400

    More UI tweaks to plans#show

--- jaribio/app/helpers/application_helper.rb
+++ jaribio/app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def status_helper(status)
+  def status_class_helper(status)
     case status
     when Status::PASS
       "status pass"
@@ -8,5 +8,38 @@ module ApplicationHelper
     else
       "status unknown"
     end
+  end
+
+  def status_checked_helper(status, expected)
+    if (status == expected)
+      return true
+    end
+    false
+  end
+
+  def status_disabled_helper(status = Status::UNKNOWN)
+    if (status == Status::UNKNOWN)
+      return false
+    end
+    true
+  end
+
+  # Copied from Rails 3.1
+  def button_tag(label = "Button", options = {})
+    options.stringify_keys!
+
+    if disable_with = options.delete("disable_with")
+      options["data-disable-with"] = disable_with
+    end
+
+    if confirm = options.delete("confirm")
+      options["data-confirm"] = confirm
+    end
+
+    ["type", "name"].each do |option|
+      options[option] = "button" unless options[option]
+    end
+
+    content_tag :button, label.to_s.html_safe, { "type" => options.delete("type") }.update(options)
   end
 end
