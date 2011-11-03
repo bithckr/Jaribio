beced7bcae44c49a087edd564274406a5afdebea
Brian Jones <bjones@mcclatchyinteractive.com>
2011-11-02 22:41:52 -0400

    Make plan#show prettier

--- jaribio/public/javascripts/application.js
+++ jaribio/public/javascripts/application.js
@@ -1,5 +1,5 @@
 function enable_execution_for_case(case_id) {
-  $('div[id="case_' + case_id + '"] form input:radio').removeAttr('disabled');
+  $('div[id="case_' + case_id + '"] input:radio').removeAttr('disabled');
 }
 function toggle_case_details(case_id) {
   $('div[id="case_' + case_id + '"] .case-details').slideToggle('slow', function() {
