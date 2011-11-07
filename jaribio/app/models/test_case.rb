6c6712fe71be18e29f425502960b60acebf4a118
Brian Jones <bjones@mcclatchyinteractive.com>
2011-11-06 22:45:16 -0500

    Fix for issue 24
    * app/models/test_case.rb (last_execution): made public, now returns
    an execution, not a relation, takes optional plan_id argument
    (status): uses Status::UNKNOWN for default instead of 0 and no longer
    needs to apply a where condition to last_execution
    * app/views/plans/_test_case.html.erb: add last execution results to
    display if available

--- jaribio/app/models/test_case.rb
+++ jaribio/app/models/test_case.rb
@@ -13,34 +13,34 @@ class TestCase < ActiveRecord::Base
     end
   end
 
-  def status(plan_id = nil)
-    status = 0
-
-    if plan_id.nil?
-      execution = last_execution
-    else
-      execution = last_execution.where("plan_id = ?", plan_id)
-    end
-    
-    unless execution.nil? or execution.empty?
-      status = execution[0].status_code
-    end
-
-    return status
-  end
-
   scope :in_plan, lambda{ |plan|
     joins(:suites => :plans ).
     where("plans.id = ?", plan.id).
     select("DISTINCT `test_cases`.*") # kill duplicates
   }
 
-  private
+  def status(plan_id = nil)
+    status = Status::UNKNOWN
 
-  # Making this private because it is subject to change along with status(), and
-  # I don't want this part of the public API for TestCase.
-  def last_execution
-    self.executions.order("created_at desc").limit(1)
+    if plan_id.nil?
+      execution = last_execution
+    else
+      execution = last_execution(plan_id)
+    end
+    
+    unless execution.nil?
+      status = execution.status_code
+    end
+
+    return status
+  end
+
+  def last_execution(plan_id = nil)
+    rel = self.executions.order("created_at desc").limit(1)
+    if (plan_id)
+      rel.where("plan_id = ?", plan_id)
+    end
+    return rel.first
   end
 
 end
