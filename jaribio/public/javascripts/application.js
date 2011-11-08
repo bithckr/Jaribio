1a900390f4941842790c84cd7255bbd31065a521
Clint <bithckr@gmail.com>
2011-11-08 18:54:33 -0500

    Changing the re-test button so it is disabled after you click on it.

--- jaribio/public/javascripts/application.js
+++ jaribio/public/javascripts/application.js
@@ -2,6 +2,7 @@ function enable_execution_for_case(case_id) {
   $('div[id="case_' + case_id + '"] input:radio').removeAttr('disabled');
   $('div[id="case_' + case_id + '"] textarea').removeAttr('disabled');
   $('div[id="case_' + case_id + '"] input:submit').removeAttr('disabled');
+  $('div[id="case_' + case_id + '"] button').attr('disabled', 'disabled');
 }
 function toggle_case_details(case_id) {
   $('div[id="case_' + case_id + '"] .case-details').slideToggle('slow', function() {
