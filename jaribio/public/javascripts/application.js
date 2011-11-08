332977378fb7ae1149db7082ca1d34df0e11f5d9
Brian Jones <bjones@mcclatchyinteractive.com>
2011-11-08 09:57:42 -0500

    Flash and disabling on plans#show

--- jaribio/public/javascripts/application.js
+++ jaribio/public/javascripts/application.js
@@ -1,5 +1,7 @@
 function enable_execution_for_case(case_id) {
   $('div[id="case_' + case_id + '"] input:radio').removeAttr('disabled');
+  $('div[id="case_' + case_id + '"] textarea').removeAttr('disabled');
+  $('div[id="case_' + case_id + '"] input:submit').removeAttr('disabled');
 }
 function toggle_case_details(case_id) {
   $('div[id="case_' + case_id + '"] .case-details').slideToggle('slow', function() {
