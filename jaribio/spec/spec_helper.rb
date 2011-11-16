7c093742f706679c4788bd8b46ee61089be64ce7
Brian Jones <bjones@mcclatchyinteractive.com>
2011-11-15 21:28:34 -0500

    Support for jruby

--- jaribio/spec/spec_helper.rb
+++ jaribio/spec/spec_helper.rb
@@ -1,5 +1,8 @@
-require 'simplecov'
-SimpleCov.start 'rails'
+begin
+  require 'simplecov'
+  SimpleCov.start 'rails'
+rescue LoadError
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
