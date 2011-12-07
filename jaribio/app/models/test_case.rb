54c30b006e49a85c5df93feb87c8a3521256063d
Brian Jones <bjones@mcclatchyinteractive.com>
2011-12-07 16:20:07 -0500

    Issue #34, add join model
    * Gemfile: added some commented out lines for using a 1.9 compatiable ruby
    debug, which is a bit of a pain in the ass at the moment
    * app/models/suite.rb: changed has_and_belongs_to_many to a has_many
    :through for test_cases
    * app/models/test_case.rb: ditto, but for suites
    * app/models/suite_test_case.rb: new file
    * spec/factories/suite_test_case.rb: new file
    * spec/models/suite_test_case_spec.rb: new file

--- jaribio/app/models/test_case.rb
+++ jaribio/app/models/test_case.rb
@@ -1,5 +1,6 @@
 class TestCase < ActiveRecord::Base
-  has_and_belongs_to_many :suites
+  has_many :suite_test_cases
+  has_many :suites, :through => :suite_test_cases
   has_many :executions
   belongs_to :user
 
