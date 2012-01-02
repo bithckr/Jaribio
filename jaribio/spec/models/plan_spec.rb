ed3196d418456e3b24d8f3a93940f12e57257aa6
Brian Jones <bjones@mcclatchyinteractive.com>
2012-01-02 17:11:54 -0500

    Speeding up tests

--- jaribio/spec/models/plan_spec.rb
+++ jaribio/spec/models/plan_spec.rb
@@ -7,8 +7,6 @@ describe Plan do
     @test_case = Factory.build :test_case, :suites => [@suite]
     @execution = Factory.build :execution, :plan => @plan, :test_case => @test_case
     @plan.executions = [@execution]
-    @user = Factory.build :user
-    @plan.user = @user
   end
 
   it "has many suites" do
@@ -20,48 +18,41 @@ describe Plan do
   end
 
   it "has a user" do
-    @plan.user.should eq(@user)
+    @plan.should respond_to(:user)
   end
 
   it "can be searched" do
-    @plan.save!
-    plans = Plan.search(@plan.name)
-    plans.size.should eq(1)
+    sql = Plan.search(@plan.name).to_sql
+    sql.should == %Q{SELECT `plans`.* FROM `plans`  WHERE (`plans`.`name` LIKE '%#{@plan.name}%')}
   end
 
   it "search accepts a relation" do
-    @plan.save!
-    plans = Plan.search(@plan.name, Plan.scoped)
-    plans.size.should eq(1)
+    sql = Plan.search(@plan.name, Plan.scoped).to_sql
+    sql.should == %Q{SELECT `plans`.* FROM `plans`  WHERE (`plans`.`name` LIKE '%#{@plan.name}%')}
   end
 
   it "can list unrelated suites" do
-    @plan.save!
-    suites = @plan.available_suites
-    suites.size.should eq(0)
-
-    Factory.create :suite
-    suites = @plan.available_suites
-    suites.size.should eq(1)
+    @plan.should respond_to(:available_suites)
+    sql = @plan.available_suites.to_sql
+    sql.should == %Q{SELECT `suites`.* FROM `suites`  WHERE (`suites`.`id` NOT IN (SELECT `suites`.`id` FROM `suites` INNER JOIN `plans_suites` ON `suites`.`id` = `plans_suites`.`suite_id` WHERE `plans_suites`.`plan_id` IS NULL))}
   end
 
   describe "with one execution per test case" do
-
     it "that passed should have a passing status" do
-      @suite.save!
       @plan.save!
-      @test_case.save!
-      @execution.save!
+      @plan.reload
       @plan.status[0].should eq(Status::PASS)
       @plan.status[1].should eq(1)
       @plan.status[2].should eq(0)
       @plan.status[3].should eq(0)
+      @plan.executions.size.should eq(1)
     end
 
     it "that failed should have a failing status" do
       fail_exec = Factory.build(:execution, :plan => @plan, :status_code => Status::FAIL)
       @plan.executions << fail_exec
       @plan.save!
+      @plan.reload
       @plan.status[0].should eq(Status::FAIL)
       @plan.status[1].should eq(1)
       @plan.status[2].should eq(1)
@@ -72,19 +63,17 @@ describe Plan do
 
   describe "with multiple executions per test case" do
     before(:each) do
-      @suite = Factory.build(:suite)
-      @plan = Factory.build(:plan, :suites => [@suite])
-      @test_case = Factory.build(:test_case, :suites => [@suite])
+      @plan.executions = []
       @pass_execution = Factory.build(:execution, :plan => @plan, :test_case => @test_case)
       @fail_execution = Factory.build(:execution, :status_code => Status::FAIL, :plan => @plan, :test_case => @test_case)
     end
 
     it "where test case passed then fails" do
       @plan.save!
-      @test_case.save!
+      @pass_execution.created_at = 1.day.ago
       @pass_execution.save!
-      sleep(2)
       @fail_execution.save!
+      @plan.reload
       @plan.status[0].should eq(Status::FAIL)
       @plan.status[1].should eq(0)
       @plan.status[2].should eq(1)
@@ -93,12 +82,11 @@ describe Plan do
     end
 
     it "where test case failed then passes" do
-      @suite.save!
       @plan.save!
-      @test_case.save!
+      @fail_execution.created_at = 1.day.ago
       @fail_execution.save!
-      sleep(2)
       @pass_execution.save!
+      @plan.reload
       @plan.status[0].should eq(Status::PASS)
       @plan.status[1].should eq(1)
       @plan.status[2].should eq(0)
@@ -108,7 +96,6 @@ describe Plan do
   end
 
   describe "with no executed test case" do
-
     it "should have a unknown status" do
       @plan.status[0].should eq(Status::UNKNOWN)
       @plan.status[1].should eq(0)
@@ -118,21 +105,12 @@ describe Plan do
   end
 
   describe "with some test cases not executed" do
-    before(:all) do
-      @suite = Factory.build(:suite)
-      @plan = Factory.build(:plan, :suites => [@suite])
-      @test_case = Factory.build(:test_case, :suites => [@suite])
-    end
-
     it "should have a unknown status when any test case is unknown" do
       pass_execution = Factory.build(:execution, :plan => @plan, :test_case => @test_case)
       test_case2 = Factory.build(:test_case, :suites => [@suite])
-      @suite.save!
-      @plan.save!
-      @test_case.save!
-      pass_execution.save!
-      test_case2.save!
       @suite.test_cases = [@test_case, test_case2]
+      @plan.executions << pass_execution
+      @plan.save!
       @plan.status[0].should eq(Status::UNKNOWN)
       @plan.status[1].should eq(1)
       @plan.status[2].should eq(0)
@@ -141,11 +119,8 @@ describe Plan do
 
     it "should have a unknown status when all test cases are unknown" do
       test_case2 = Factory.build(:test_case, :suites => [@suite])
-      @suite.save!
-      @plan.save!
-      @test_case.save!
-      test_case2.save!
       @suite.test_cases = [@test_case, test_case2]
+      @plan.save!
       @plan.status[0].should eq(Status::UNKNOWN)
       @plan.status[1].should eq(1)
       @plan.status[2].should eq(0)
@@ -155,12 +130,9 @@ describe Plan do
     it "should have a fail status when atleast one test case is failing" do
       fail_execution = Factory.build(:execution, :status_code => Status::FAIL, :plan => @plan, :test_case => @test_case)
       test_case2 = Factory.build(:test_case, :suites => [@suite])
-      @suite.save!
-      @plan.save!
-      @test_case.save!
-      fail_execution.save!
-      test_case2.save!
       @suite.test_cases = [@test_case, test_case2]
+      @plan.executions << fail_execution
+      @plan.save!
       @plan.status[0].should eq(Status::FAIL)
       @plan.status[1].should eq(0)
       @plan.status[2].should eq(1)
@@ -169,23 +141,18 @@ describe Plan do
   end
 
   describe "with multiple test cases per suite" do
-    before(:all) do
-      @suite = Factory.build(:suite)
-      @plan = Factory.build(:plan, :suites => [@suite])
-      @test_case = Factory.build(:test_case, :suites => [@suite])
+    before(:each) do
+      @plan.executions = []
     end
 
     it "has a failing status if all are failing" do
       fail_execution = Factory.build(:execution, :status_code => Status::FAIL, :plan => @plan, :test_case => @test_case)
       test_case2 = Factory.build(:test_case, :suites => [@suite])
       fail_execution2 = Factory.build(:execution, :status_code => Status::FAIL, :plan => @plan, :test_case => test_case2)
-      @suite.save!
-      @plan.save!
-      @test_case.save!
-      fail_execution.save!
-      fail_execution2.save!
-      test_case2.save!
       @suite.test_cases = [@test_case, test_case2]
+      @plan.executions << fail_execution
+      @plan.executions << fail_execution2
+      @plan.save!
       @plan.status[0].should eq(Status::FAIL)
       @plan.status[1].should eq(0)
       @plan.status[2].should eq(2)
@@ -196,13 +163,10 @@ describe Plan do
       pass_execution = Factory.build(:execution, :plan => @plan, :test_case => @test_case)
       test_case2 = Factory.build(:test_case, :suites => [@suite])
       pass_execution2 = Factory.build(:execution, :plan => @plan, :test_case => test_case2)
-      @suite.save!
-      @plan.save!
-      @test_case.save!
-      pass_execution.save!
-      pass_execution2.save!
-      test_case2.save!
       @suite.test_cases = [@test_case, test_case2]
+      @plan.executions << pass_execution
+      @plan.executions << pass_execution2
+      @plan.save!
       @plan.status[0].should eq(Status::PASS)
       @plan.status[1].should eq(2)
       @plan.status[2].should eq(0)
@@ -213,13 +177,10 @@ describe Plan do
       fail_execution = Factory.build(:execution, :status_code => Status::FAIL, :plan => @plan, :test_case => @test_case)
       test_case2 = Factory.build(:test_case, :suites => [@suite])
       pass_execution2 = Factory.build(:execution, :plan => @plan, :test_case => test_case2)
-      @suite.save!
-      @plan.save!
-      @test_case.save!
-      fail_execution.save!
-      pass_execution2.save!
-      test_case2.save!
       @suite.test_cases = [@test_case, test_case2]
+      @plan.executions << fail_execution
+      @plan.executions << pass_execution2
+      @plan.save!
       @plan.status[0].should eq(Status::FAIL)
       @plan.status[1].should eq(1)
       @plan.status[2].should eq(1)
--- jaribio/spec/models/suite_spec.rb
+++ jaribio/spec/models/suite_spec.rb
@@ -24,42 +24,30 @@ describe Suite do
   end
 
   it "can be searched" do
-    @suite.save!
-    suites = Suite.search(@suite.name)
-    suites.size.should eq(1)
+    sql = Suite.search(@suite.name).to_sql
+    sql.should == %Q{SELECT `suites`.* FROM `suites`  WHERE (`suites`.`name` LIKE '%#{@suite.name}%')}
   end
 
   it "search accepts a relation" do
-    @suite.save!
-    suites = Suite.search(@suite.name, Suite.scoped)
-    suites.size.should eq(1)
+    sql = Suite.search(@suite.name, Suite.scoped).to_sql
+    sql.should == %Q{SELECT `suites`.* FROM `suites`  WHERE (`suites`.`name` LIKE '%#{@suite.name}%')}
   end
 
   it "can list unrelated test cases" do
-    @suite.save!
-    cases = @suite.available_test_cases
-    cases.size.should eq(0)
-
-    Factory.create :test_case
-    cases = @suite.available_test_cases
-    cases.size.should eq(1)
+    sql = @suite.available_test_cases.to_sql
+    sql.should == %Q{SELECT `test_cases`.* FROM `test_cases`  WHERE (`test_cases`.`id` NOT IN (SELECT `test_cases`.`id` FROM `test_cases` INNER JOIN `suites_test_cases` ON `test_cases`.`id` = `suites_test_cases`.`test_case_id` WHERE `suites_test_cases`.`suite_id` IS NULL))}
   end
 
   describe "with one execution per test case" do
-
     it "that passed should have a passing status" do
       @plan.save!
-      @case.save!
-      @suite.save!
-      @suite.status(@plan_id).should eq(Status::PASS)
+      @suite.status(@plan.id).should eq(Status::PASS)
     end
 
     it "that failed should have a failing status" do
       fail_exec = Factory.build(:execution, :plan => @plan, :test_case => @case, :status_code => Status::FAIL)
       @plan.executions << fail_exec
       @plan.save!
-      @case.save!
-      @suite.save!
       @suite.status(@plan.id).should eq(Status::FAIL)
       @plan.executions.size.should eq(2)
     end
@@ -79,8 +67,8 @@ describe Suite do
       @plan.save!
       @test_case.save!
       @pass_execution.save!
-      sleep(2)
       @fail_execution.save!
+      @plan.reload
       @suite.status(@plan.id).should eq(Status::FAIL)
       @plan.executions.size.should eq(2)
     end
@@ -89,8 +77,8 @@ describe Suite do
       @plan.save!
       @test_case.save!
       @fail_execution.save!
-      sleep(2)
       @pass_execution.save!
+      @plan.reload
       @suite.status(@plan.id).should eq(Status::PASS)
       @plan.executions.size.should eq(2)
     end
