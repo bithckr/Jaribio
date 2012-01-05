e7e8b02fa144ac9f58dd6f92889ea8736852a09d
Clint <bithckr@gmail.com>
2012-01-05 16:17:06 -0500

    Removed button_tag from helper, now part of rails 3.1.

--- jaribio/app/helpers/application_helper.rb
+++ jaribio/app/helpers/application_helper.rb
@@ -34,23 +34,4 @@ module ApplicationHelper
     end
     true
   end
-
-  # Copied from Rails 3.1
-  def button_tag(label = "Button", options = {})
-    options.stringify_keys!
-
-    if disable_with = options.delete("disable_with")
-      options["data-disable-with"] = disable_with
-    end
-
-    if confirm = options.delete("confirm")
-      options["data-confirm"] = confirm
-    end
-
-    ["type", "name"].each do |option|
-      options[option] = "button" unless options[option]
-    end
-
-    content_tag :button, label.to_s.html_safe, { "type" => options.delete("type") }.update(options)
-  end
 end
