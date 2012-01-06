bdc9d2199138d6ec5e42a466cd0f306c27482fe8
Clint <bithckr@gmail.com>
2012-01-06 09:53:15 -0500

    Adding a controller test for executions.

--- jaribio/spec/controllers/executions_controller_spec.rb
+++ jaribio/spec/controllers/executions_controller_spec.rb
@@ -15,4 +15,22 @@ describe ExecutionsController do
     end
   end
 
+  describe "POST create" do
+    before(:each) do
+      @test_case = Factory.create(:test_case)
+      @suite = Factory.create(:suite)
+      @suite.test_cases = [@test_case]
+      @plan = Factory.create(:plan)
+      @plan.suites = [@suite]
+    end
+
+    describe "with valid params" do
+      it "creates a new execution" do
+        expect {
+          post :create, :execution => Factory.attributes_for(:execution), :test_case_id => @test_case.id.to_s, :test_case_counter => 1, :suite_id => @suite.id.to_s, :plan_id => @plan.id.to_s
+        }.to change(Execution, :count).by(1)
+      end
+    end
+  end
+
 end
