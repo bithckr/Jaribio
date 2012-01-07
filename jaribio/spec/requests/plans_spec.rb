34b33335bc0508459073d47c86d9262c15636a11
Brian Jones <bjones@mcclatchyinteractive.com>
2012-01-06 22:42:00 -0500

    Writing some tests
    * spec/requests/plans_spec.rb: new shared example for plan create/edit
    form, new tests for /plans/new, /plans/1/edit
    * spec/requests/suites_spec.rb: uses shared example for secondary
    navigation tests
    * spec/requests/test_cases_spec.rb ditto

--- jaribio/spec/requests/plans_spec.rb
+++ jaribio/spec/requests/plans_spec.rb
@@ -1,5 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+shared_examples "a form to create/edit a plan" do
+  before(:each) do
+    visit path
+  end
+
+  it "can enter plan 'Name'" do
+    page.should have_field("plan_name")
+  end
+
+  it "has 'Save' button" do
+    page.should have_button("Save")
+  end
+
+  it "has 'Cancel' link" do
+    page.should have_link("Cancel")
+  end
+end
+
 describe "Plans" do
 
   describe "GET /plans" do
@@ -52,11 +70,31 @@ describe "Plans" do
 
   end
 
+  describe "GET /plans/new" do
+    before(:each) do 
+      @user = login_any_user
+    end
+
+    it_behaves_like "a page with secondary navigation" do
+      let(:path) { new_plan_path }
+    end
+
+    it_behaves_like "a form to create/edit a plan" do 
+      let(:path) { new_plan_path }
+    end
+
+  end
+
   describe "GET /plans/1" do
     before(:each) do
       @user = login_any_user
       @plan = Factory.create(:plan)
     end
+
+    it_behaves_like "a page with secondary navigation" do 
+      let(:path) { plan_path(@plan) }
+    end
+
     describe "show" do
       before(:each) do
         visit plan_path(@plan)
@@ -68,10 +106,47 @@ describe "Plans" do
     end
   end
 
+  describe "GET /plans/1/edit" do
+    before(:each) do
+      @user = login_any_user
+      @plan = Factory.create(:plan)
+    end
+
+    it_behaves_like "a page with secondary navigation" do
+      let(:path) { edit_plan_path(@plan) }
+    end
+
+    it_behaves_like "a form to create/edit a plan" do 
+      let(:path) { edit_plan_path(@plan) }
+    end
+
+    describe "current suite list" do
+      it "is empty if no suites are associated to the plan" do
+        visit edit_plan_path(@plan)
+        page.should_not have_link('Unassociate')
+      end
+
+      it "is displayed if suites are associated to the plan" do
+        @suite = Factory.create(:suite)
+        @plan.suites = [@suite]
+        @plan.save!
+        visit edit_plan_path(@plan)
+        page.should have_content('Current Suites')
+        page.should have_link('Unassociate')
+      end
+
+      it "supports search through suite list"
+    end
+  end
+
   describe "GET /plans/1/add_suites" do
     before(:each) do
       @user = login_any_user
       @plan = Factory.create(:plan)
+    end
+
+    it_behaves_like "a page with secondary navigation" do 
+      let(:path) { url_for([:add_suites, @plan]) }
     end
 
     it "with no associated suites" do
