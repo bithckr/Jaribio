3946ff1d7f79b98110da2547e320b20e1d765832
Brian Jones <bjones@mcclatchyinteractive.com>
2012-01-25 22:12:10 -0500

    Issue #14 get ready to test
    * Gemfile.lock: generated and checked in
    * Rakefile: remove rcov task and clear out a couple of release tasks
    that would be a bad idea to run in Jaribio
    * VERSION: new file
    * lib/rspec_formatter.rb: just commenting out the require ruby-debug
    * spec/lib/rspec_formatter_spec.rb: starting to write a real test

--- jaribio-formatter/lib/rspec_formatter.rb
+++ jaribio-formatter/lib/rspec_formatter.rb
@@ -1,4 +1,4 @@
-require 'ruby-debug'
+#require 'ruby-debug'
 
 module Jaribio
   class RSpecFormatter
--- jaribio-formatter/spec/lib/rspec_formatter_spec.rb
+++ jaribio-formatter/spec/lib/rspec_formatter_spec.rb
@@ -1,4 +1,13 @@
-require File.expand_path(File.dirname(__FILE__) + '../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe "RSpecFormatter" do
+describe "Jaribio::RSpecFormatter" do
+  let(:output) { StringIO.new }
+  let(:formatter) { Jaribio::RSpecFormatter.new(output) }
+
+  it "gets jaribio key" do
+    group = RSpec::Core::ExampleGroup.describe("object")
+    group.example("example 1")
+    group.run(RSpec::Core::Reporter.new(formatter))
+    fail "fix this test"
+  end
 end
