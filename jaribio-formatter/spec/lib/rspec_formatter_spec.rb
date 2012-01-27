7408d557a5b825e81eae730df08286d698afb48e
Brian Jones <bjones@mcclatchyinteractive.com>
2012-01-27 07:22:29 -0500

    A working test case!
    * jaribio-formatter/spec/lib/rspec_formatter_spec.rb: woke up
    realizing what needed to be done, so here is the first working
    test case

--- jaribio-formatter/spec/lib/rspec_formatter_spec.rb
+++ jaribio-formatter/spec/lib/rspec_formatter_spec.rb
@@ -4,10 +4,19 @@ describe "Jaribio::RSpecFormatter" do
   let(:output) { StringIO.new }
   let(:formatter) { Jaribio::RSpecFormatter.new(output) }
 
-  it "gets jaribio key" do
+  it "gets jaribio_key from example"
+  it "gets jaribio_key from example group"
+  it "gets jaribio_key from parent group"
+
+  it "uses example group full description as key" do
     group = RSpec::Core::ExampleGroup.describe("object")
-    group.example("example 1")
-    group.run(RSpec::Core::Reporter.new(formatter))
-    fail "fix this test"
+    example = group.example("example 1")
+    # group.run(RSpec::Core::Reporter.new(formatter))
+    formatter.instance_eval { 
+      @example_group = group
+    }
+    key, desc = formatter.send(:get_example_key, example)
+    key.should == "object example 1"
+    desc.should == "object example 1"
   end
 end
