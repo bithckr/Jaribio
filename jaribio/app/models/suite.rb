14eecd5807602c5948acfb8df7b9684aaf185d39
Clint <bithckr@gmail.com>
2012-01-29 18:57:03 -0500

    Issue #58. Fixing suite.status. It now respects plan.closed_at if called within a plan context. Fixed a bug in Plan#show, when rendering the suites, we were rendering the suite status text without the plan context, resulting in the wrong status.

--- jaribio/app/models/suite.rb
+++ jaribio/app/models/suite.rb
@@ -27,11 +27,13 @@ class Suite < ActiveRecord::Base
   def status(plan_id = nil) 
     status = Status::UNKNOWN
     plan_context = ''
+    plan = Plan.new
 
     # If a plan_id is provided, then we restrict the suite status to this plan
     # else the suite status will rely on the last executions for its test cases.
     unless plan_id.nil?
         plan_context = "and plan_id = #{plan_id}"
+        plan = Plan.find(plan_id)
     end
 
     # Here we select the last execution per test_case in the suite, then we order the
@@ -39,25 +41,26 @@ class Suite < ActiveRecord::Base
     # inspecting only one record. If we have not recorded any executions for the
     # given suite, then we return Status::UNKNOWN.
     execution = Execution.find_by_sql(["select LEFT(GROUP_CONCAT(status_code order by " +
-                                       "created_at desc), 1) as status_code from executions " +
+                                       "executions.created_at desc), 1) as status_code from executions " +
                                        "inner join suites_test_cases on " +
                                        "executions.test_case_id=suites_test_cases.test_case_id " +
                                        "where suites_test_cases.suite_id=? #{plan_context} " +
                                        "group by executions.test_case_id " +
-                                       "order by status_code desc limit 1", self.id])
+                                       "order by status_code desc", self.id])
 
     unless execution.nil? or execution.empty?
       if execution[0].status_code == Status::FAIL
         status = Status::FAIL
       elsif execution[0].status_code == Status::PASS
-        execution = Execution.find_by_sql(["select count(executions.test_case_id) from executions " +
-                                           "inner join suites_test_cases on " +
-                                           "executions.test_case_id=suites_test_cases.test_case_id " +
-                                           "where suites_test_cases.suite_id=? #{plan_context} " +
-                                           "group by executions.test_case_id order by status_code desc", self.id])
+        if (plan.closed_at.nil?)
+         test_count = self.test_cases.size 
+        else
+         # Only count the test cases that were created before the plan was closed.
+         test_count = self.test_cases.where("created_at < ?", plan.closed_at).size
+        end
 
         # A suite can not have a PASS status unless ALL test cases are passing
-        status = Status::PASS if(execution.count == self.test_cases.size)
+        status = Status::PASS if(execution.size == test_count)
       end
     end
 
