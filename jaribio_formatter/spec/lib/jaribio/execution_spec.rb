10f2bd77e89adf4853b758c1307d06745d2079b1
Brian Jones <bjones@mcclatchyinteractive.com>
2012-04-07 21:58:47 -0400

    Issue #68 tests
    * spec/lib/jaribio/execution_spec.rb: implement 2 pending tests

--- jaribio_formatter/spec/lib/jaribio/execution_spec.rb
+++ jaribio_formatter/spec/lib/jaribio/execution_spec.rb
@@ -1,8 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "Jaribio::Result" do
+describe "Jaribio::Execution" do
   describe "#record_results" do
-    it "finds open plans"
-    it "creates new executions for open plans and existing test cases"
+    before do
+      @plan = double("Jaribio::Plan", :id => 1)
+      Jaribio::Plan.should_receive(:find).
+        with(:all, { :from => :open }).
+        and_return([@plan])
+    end
+
+    it "finds open plans" do
+      Jaribio::Execution.record_results([])
+    end
+
+    it "creates new executions for open plans and existing test cases" do
+      record = Jaribio::Record.new(:key => 'abcdef', :description => "description", :state => Jaribio::Record::PASS) 
+      records = [record]
+      test_case = double("Jaribio::TestCase", :id => 1)
+      Jaribio::TestCase.should_receive(:find).with(record.key).and_return(test_case)
+      execution = double("Jaribio::Execution")
+      execution.should_receive(:save).with({:plan_id => @plan.id, :test_case_id => test_case.id})
+      Jaribio::Execution.should_receive(:new).with({:status_code => record.state, :results => record.error}).and_return(execution)
+      Jaribio::Execution.record_results(records)
+    end
   end
 end
